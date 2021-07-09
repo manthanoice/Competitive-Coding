d28e9c360272b6f53ed0a37831fd842ddd635816
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-09 18:11:59 +0530

    Create PPSUM.java

---
+++ CodeChef/CodeChef-School/PPSUM.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static int sum(int n){
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            ans+=i;
+        }
+        return ans;
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int d = sc.nextInt();
+		    int n = sc.nextInt();
+		    for(int i=0; i<d; i++)
+		        n=sum(n);
+		    System.out.println(n);
+		}
+	}
+}
