d6bc0ecf6492f098de702fa7ead27cb44d058e67
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-11 14:53:26 +0530

    Create RNGEODD.java

---
+++ CodeChef/CodeChef-YouTube-Practice-Problems/RNGEODD.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		for(int i=n1; i<=n2; i++)
+		    if(i%2!=0)
+		        System.out.print(i+" ");
+	}
+}
