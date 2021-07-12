f9949981b1bd5eb0a331f4833e4b80124f348520
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-12 20:13:14 +0530

    Create TRIVALCH.java

---
+++ CodeChef/CodeChef-DSA-Learning-Series/Getting started Practice Contest/TRIVALCH.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		String s = (a+b>c&&b+c>a&&a+c>b)?"YES":"NO";
+		System.out.println(s);
+	}
+}
