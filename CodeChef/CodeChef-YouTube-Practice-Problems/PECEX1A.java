d441a4d46d7291629175ccc859ee55d4b7022a81
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-22 19:54:23 +0530

    Create PECEX1A.java

---
+++ CodeChef/CodeChef-YouTube-Practice-Problems/PECEX1A.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int [] n = new int [5];
+		    for(int i=0; i<n.length; i++){
+		        n[i]=sc.nextInt();
+		    }
+		    Arrays.sort(n);
+		    for(int i=0; i<n.length; i++){
+		        System.out.println(n[2]);
+		        break;
+		    }
+		}
+	}
+}
