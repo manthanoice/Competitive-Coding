002fadc6aa90dc3b12e1c42d174dbaeab3b97539
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-26 19:21:33 +0530

    Update EVENGAME.java

--- CodeChef/CodeChef-Easy/EVENGAME.java
+++ CodeChef/CodeChef-Easy/EVENGAME.java
@@ -13,7 +13,7 @@ class Codechef
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		while(t-->0){
-            int sum = 0;
+            	int sum = 0;
 		    int n = sc.nextInt();
 		    int arr[] = new int[n];
 		    for(int i=0; i<n; i++){
