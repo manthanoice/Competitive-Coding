ece5c5e32778f4c9eaf160f5ce1c5bc9cae41f6e
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-26 19:22:17 +0530

    Create CIELAB.java

---
+++ CodeChef/CodeChef-Easy/CIELAB.java
@@ -0,0 +1,64 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader()
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() { return Integer.parseInt(next()); }
+        
+        long nextLong() { return Long.parseLong(next()); }
+        
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader sc = new FastReader();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if((a-b)%10==9)
+		    System.out.println((a-b)-1);
+		else
+		    System.out.println((a-b)+1);
+	}
+}
