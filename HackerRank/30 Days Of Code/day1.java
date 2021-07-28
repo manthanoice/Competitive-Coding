f3498cd5554115ad081ecb5d06a5df07710fde95
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-28 15:44:05 +0530

    Create day1.java

---
+++ HackerRank/30 Days Of Code/day1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i=4;
+        double d=4.0;
+        String s="HackerRank ";
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        double d1 = sc.nextDouble();
+        sc.nextLine();
+        String s1 = sc.nextLine();
+        int sum = i+n;
+        double sum2 = d+d1;
+        System.out.println(sum);
+        System.out.println(sum2);
+        System.out.println(s+s1);
+    }
+}
