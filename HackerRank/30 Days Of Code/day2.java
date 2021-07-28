180d7c2bd787855df781227f90df3a3420682e18
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-28 15:44:36 +0530

    Create day2.java

---
+++ HackerRank/30 Days Of Code/day2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static void solve(double meal_cost, int tip_percent, int tax_percent) 
+{
+        double tip=meal_cost*tip_percent/100;
+        double tax=meal_cost*tax_percent/100;
+        int totalCost=(int)Math.round(meal_cost+tip+tax);
+        System.out.print(totalCost);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        
+        int tip_percent = scanner.nextInt();
+        
+        int tax_percent = scanner.nextInt();
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
