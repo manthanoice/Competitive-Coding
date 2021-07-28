34570d29a3ec2e223443f65d93dc8802964b04e3
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-28 15:43:05 +0530

    Create day1.java

---
+++ HackerRank/30 Days Of Code/day1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Hello, World.");
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
