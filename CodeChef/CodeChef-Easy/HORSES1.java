15b9a067a3b6c86c5d88940d3aeaf743b864dd7b
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-08-18 19:14:55 +0530

    First method

---
+++ CodeChef/CodeChef-Easy/HORSES1.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int arr[] = new int[n];
+		    for(int i=0; i<n; i++)
+		        arr[i] = sc.nextInt();
+		    Arrays.sort(arr);
+		    //Another way could be using Math.min I guessss
+		    int min = Integer.MAX_VALUE;
+		    for(int i=1; i<n; i++)
+		        min = Math.min(min,arr[i]-arr[i-1]);
+		    System.out.println(min);
+		}
+	}
+}
