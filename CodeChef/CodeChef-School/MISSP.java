bd12dbd2f583e0513e1d3b9378856a5808295ea3
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-08-26 23:14:19 +0530

    eh

---
+++ CodeChef/CodeChef-School/MISSP.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int arr[] = new int[n];
+		    for(int i=0; i<n; i++)
+		        arr[i] = sc.nextInt();
+		    Arrays.sort(arr);
+		    for(int i=0; i<n; i++){
+		        int count = 0;
+		        for(int j=0; j<n; j++){
+		            if(arr[i]==arr[j]){
+		                count++;
+		            }
+		        }
+		        if(count%2>0){
+		            System.out.println(arr[i]);
+		            break;
+		        }
+		    }
+		}
+	}
+}
